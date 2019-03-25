3763312b9ba7dd2cf1d7688c6f76cd25da5b285c
Jimmy Calderon <JimmyCalderon@Jimmys-MBP.fc.ca>
2019-03-25 12:18:54 -0400

    changing data one way

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -6,7 +6,15 @@
 		  lightbox 	 = 	document.querySelector('.lightbox'),
 		  video		 =	document.querySelector('video'),
 		  lbClose	 =	document.querySelector('.lightbox-close'),
-		  topBanners =  document.querySelector('#houseImages');
+		  topBanners =  document.querySelector('#houseImages'),
+		  tagline    =  document.querySelector('.house-name'),
+		  houseInfo  =  document.querySelector('.house-info');
+
+	// this is a container for some dynamic data
+	const houseData = [ //STARK
+		// backticks (javascript template string) let you keep the punctuation youre using inside of text
+		`House Stark of Winterfell is a Great House of Westeros, ruling over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and Daenerys Targaryen's invasion of Westeros, the leaders of House Stark ruled over the region as the Kings in the North.`
+	];
 
 	function openLightbox() {
 		// debugger;
@@ -17,6 +25,13 @@
 
 		// flip this to uppercase
 		let targetVid = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+
+		// populate the tagline
+		tagline.textContent = `House  ${targetVid}`;
+
+		// pupulate the house content
+		houseInfo.textContent = houseData[0];
+
 
 		video.src = `video/House-${targetVid}.mp4`;
 		lightbox.classList.add('lightbox-on');
@@ -30,6 +45,7 @@
 		// rewind the current video and pause it
 		video.currentTime = 0;
 		video.pause();
+		
 	}
 
 	function animateBanners() {
@@ -40,10 +56,10 @@
 		topBanners.style.right = currentOffset + "px";
 	}
 
-	// sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
+	sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
 
 	// animate the banners at the top
-	sigils.forEach(sigil => sigil.addEventListener('click', animateBanners));
+	//sigils.forEach(sigil => sigil.addEventListener('click', animateBanners));
 
 	video.addEventListener('ended', closeLightbox);
 	lbClose.addEventListener('click', closeLightbox);
